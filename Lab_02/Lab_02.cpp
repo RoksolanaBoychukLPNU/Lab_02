eced4f2639f91ba0bc9d50ea7c17e8502a3139a9
Roksolana <roksolanaboychuk8@gmail.com>
2026-09-15 23:16:40 +0300

    first commit

---
+++ Lab_02/Lab_02.cpp
@@ -0,0 +1,30 @@
+// Lab_02.cpp
+// Бойчук Роксолана
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 2
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double Pi = 4 * atan(1);   
+    double a;               
+    double z1;                  
+    double z2;                  
+
+    cout << "a = "; 
+    cin >> a;
+
+    z1 = cos(a) + sin(a) + cos(3 * a) + sin(3 * a);
+    z2 = 2 * sqrt(2) * cos(a) * sin(2 * a + Pi / 4);
+
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    return 0;
+}
